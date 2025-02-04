89e06fdae7c6039c7466635d79b40914d61c5377
Hisham Thajudheen <hishamthajudheen@gmail.com>
2025-02-05 03:37:36 +0530

    changed some function based views to class based views

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView
 
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
@@ -23,12 +23,18 @@ def lead_list(request):
     }
     return render(request, 'leads/lead_list.html', context)
 
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {
         "lead": lead
     }
     return render(request, "leads/lead_detail.html", context)
+
 
 def lead_create(request):
     form = LeadModelForm()
@@ -59,6 +65,13 @@ def lead_update(request, pk):
         "lead": lead,
     }
     return render(request, "leads/lead_update.html", context)
+
+class LeadDeleteView(DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all()
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
 
 def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)
