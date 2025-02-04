2cb53f70b818e2587c7cbfd64121bdc2d83a5922
Hisham Thajudheen <hishamthajudheen@gmail.com>
2025-02-04 20:07:42 +0530

    Initial commit

--- djcrm2/urls.py
+++ djcrm2/urls.py
@@ -1,4 +1,4 @@
-"""djcrm URL Configuration
+"""djcrm2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- djcrm2/wsgi.py
+++ djcrm2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djcrm project.
+WSGI config for djcrm2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcrm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcrm2.settings')
 
 application = get_wsgi_application()
