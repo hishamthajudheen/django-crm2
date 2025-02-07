48132825d46d93d1e99ec5de7b640827b9b48e06
Hisham Thajudheen <hishamthajudheen@gmail.com>
2025-02-08 00:53:13 +0530

    deployment files setupd

--- djcrm2/settings.py
+++ djcrm2/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 ALLOWED_HOSTS = [
+    '*',
     'django-crm2.onrender.com',
 ]
 
@@ -160,3 +161,15 @@ LOGIN_URL = "login"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = "DENY"
